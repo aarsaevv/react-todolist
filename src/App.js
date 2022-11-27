654754f914d612157c1543bc54b3f08f1b9e545a
aarsaevv <arsaevgeny@gmail.com>
2022-11-27 21:52:42 +0600

    Перенос из pathetic-backend

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import TodoList from "./components/TodoList/TodoList.js";
 function App() {
 	let [todos, setTodos] = useState([]);
 
+<<<<<<< Updated upstream
 	const addTodo = (todo) => {
 		todos.push(todo);
 		setTodos(todos);
@@ -13,6 +14,56 @@ function App() {
 	};
 	const removeTodo = (todo) => {
 		setTodos(todos.filter((el) => todo.id !== el.id));
+=======
+	// Firebase Configuration
+	const firebaseConfig = {
+		apiKey: "AIzaSyB-lcuq78eO4MQv9kTu9Ys9sV6881QXeDA",
+		authDomain: "react-todolist-bc761.firebaseapp.com",
+		projectId: "react-todolist-bc761",
+		storageBucket: "react-todolist-bc761.appspot.com",
+		messagingSenderId: "701744540272",
+		appId: "1:701744540272:web:7ef97f060cb3995a2422b5",
+	};
+	// Initialize Firebase
+	const app = initializeApp(firebaseConfig);
+	// Initialize Cloud Firestore and get a reference to the service
+	const db = getFirestore(app);
+
+	useEffect(() => {
+		loadTodosFromDatabase();
+	}, []);
+
+	const loadTodosFromDatabase = async (todo) => {
+		const querySnapshot = await getDocs(collection(db, "todos"));
+		console.log(querySnapshot.docs[0].id); // Пригодится
+		fetch(
+			"https://firestore.googleapis.com/v1/projects/react-todolist-bc761/databases/(default)/documents/todos"
+		)
+			.then((response) => response.json())
+			.then((json) => {
+				let fields = json.documents.map((document) => {
+					return {
+						checked: document.fields.checked.booleanValue,
+						creatingTime: document.fields.creatingTime.integerValue,
+						deadlineTime: document.fields.deadlineTime.integerValue,
+						description: document.fields.description.stringValue,
+						id: document.name,
+						imageSrc: document.fields.imageSrc.stringValue,
+						title: document.fields.title.stringValue,
+					};
+				});
+				setTodos(fields);
+			});
+	};
+
+	const addTodo = async (todo) => {
+		await addDoc(collection(db, "todos"), todo);
+		loadTodosFromDatabase();
+	};
+	const removeTodo = async (todo) => {
+		// await deleteDoc(doc(db, "todos", todo.id));
+		// loadTodosFromDatabase();
+>>>>>>> Stashed changes
 	};
 	const editTodoTitle = (todo) => {
 		todos = todos.map((item, index) => {
--- src/components/Forms/TodoAddForm.js
+++ src/components/Forms/TodoAddForm.js
@@ -1,5 +1,10 @@
 import { useState } from "react";
+<<<<<<< Updated upstream
 import dayjs from "dayjs";
+=======
+import dayjs from "dayjs"
+import EmbeddedFiles from "./EmbeddedFiles.js";
+>>>>>>> Stashed changes
 
 function TodoAddForm(props) {
 	const [title, setTitle] = useState("");
