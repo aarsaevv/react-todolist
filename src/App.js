c49c0411055f95b5adaf9bc280c5ecd097f038ed
aarsaevv <arsaevgeny@gmail.com>
2022-12-19 23:45:51 +0600

    Наконец-то это работает нормально

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ function App() {
 
 	/** Конфигурация Firebase,
 	 * 	Инициализация Cloud и получение ссылки для работы с БД */
-
 	const firebaseConfig = {
 		apiKey: "AIzaSyB-lcuq78eO4MQv9kTu9Ys9sV6881QXeDA",
 		authDomain: "react-todolist-bc761.firebaseapp.com",
@@ -30,91 +29,62 @@ function App() {
 	};
 	const app = initializeApp(firebaseConfig);
 	const db = getFirestore(app);
+
 	/** Используется хук useEffect. React загружает тудус после того, как модифицируется DOM-дерево **/
 	useEffect(() => {
 		loadTodosFromDatabase();
 	}, []);
-<<<<<<< Updated upstream
 
-	/** Загрузка тудус через фетч. Правильнее будет сделать это через запрос из библиотеки */
-=======
-<<<<<<< Updated upstream
-	// DONE
->>>>>>> Stashed changes
-	const loadTodosFromDatabase = async (todo) => {
-		fetch(
-			"https://firestore.googleapis.com/v1/projects/react-todolist-bc761/databases/(default)/documents/todos"
-		)
-			.then((response) => response.json())
-			.then((json) => {
-				if (json.documents) {
-					/** Маппинг приходящего из дб JSON и подставление его полей в поля нашего объекта */
-					let fields = json.documents.map((document) => {
-						return {
-							checked: document.fields.checked.booleanValue,
-							creatingTime: document.fields.creatingTime.integerValue,
-							deadlineTime: document.fields.deadlineTime.integerValue,
-							description: document.fields.description.stringValue,
-							id: document.name,
-							title: document.fields.title.stringValue,
-							file: document.fields.file.stringValue,
-						};
-					});
-					/** Сетаем готовый объект в todos */
-					setTodos(fields);
-				} else setTodos("");
-=======
-
-	/** Загрузка тудус через фетч. Правильнее будет сделать это через запрос из библиотеки */
 	const loadTodosFromDatabase = async () => {
 		const querySnapshot = await getDocs(collection(db, "todos"));
 		let arrayOfDocuments = [];
 		let arrayOfIDs = [];
 		if (querySnapshot) {
 			querySnapshot.forEach((doc) => {
-				arrayOfDocuments.push(doc.data());
 				arrayOfIDs.push(doc.id);
->>>>>>> Stashed changes
+				arrayOfDocuments.push(doc.data());
 			});
-			console.log(arrayOfDocuments);
-			console.log(arrayOfIDs);
+			arrayOfDocuments.map((document, index) => {
+				document.id = arrayOfIDs[index];
+				return document.id;
+			});
 			setTodos(arrayOfDocuments);
 		} else {
 			setTodos("");
 		}
 	};
+
 	/** Добавление нового документа в коллекцию todos */
 	const addTodo = async (todo) => {
 		await addDoc(collection(db, "todos"), todo);
 		await loadTodosFromDatabase();
 	};
+
 	/** Удаление документа по id из коллекции todos */
 	const removeTodo = async (todo) => {
-		let realDocumentId = todo.id.slice;
-		await deleteDoc(doc(db, "todos", realDocumentId));
+		await deleteDoc(doc(db, "todos", todo.id));
 		loadTodosFromDatabase();
 	};
+
 	/** Переписывание полей объекта приходящими данными - Title и Description */
 	const editTodoTitle = async (todo) => {
 		if (todo.id) {
-			let realDocumentId = todo.id.slice(-20);
-			const todoRef = doc(db, "todos", realDocumentId);
+			const todoRef = doc(db, "todos", todo.id);
 			await setDoc(todoRef, { title: todo.title }, { merge: true });
 			await loadTodosFromDatabase();
 		}
 	};
 	const editTodoDescription = async (todo) => {
 		if (todo.id) {
-			let realDocumentId = todo.id.slice(-20);
-			const todoRef = doc(db, "todos", realDocumentId);
+			const todoRef = doc(db, "todos", todo.id);
 			await setDoc(todoRef, { description: todo.description }, { merge: true });
 			await loadTodosFromDatabase();
 		}
 	};
+
 	/** Чекинг тудушки и замена на "сделано" при клике */
 	const toggleChecked = async (todo) => {
-		let realDocumentId = todo.id.slice(-20);
-		const todoRef = doc(db, "todos", realDocumentId);
+		const todoRef = doc(db, "todos", todo.id);
 		const todoSnap = await getDoc(todoRef);
 		let objectData = todoSnap.data();
 		if (objectData.checked === false) {
